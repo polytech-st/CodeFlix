b3e7a4f44d55bb4024cab2155c0cb2ad07efe70a
alyona-kyrian <olena.kyrian@polytech.pp.ua>
2025-05-23 18:25:14 +0300

    configure

--- script.js
+++ script.js
@@ -8,4 +8,35 @@ fetch("../Header/header.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("header").innerHTML = data;
-  });
+  }); 
+
+  document.getElementById('searchInput').addEventListener('input', function() {
+    const query = this.value.toLowerCase();
+    const elements = document.querySelectorAll('[data-name]');
+
+    elements.forEach(el => {
+        const name = el.getAttribute('data-name').toLowerCase();
+        if (name.includes(query)) {
+            el.style.display = ''; // показати
+        } else {
+            el.style.display = 'none'; // приховати
+        }
+    });
+});
+
+  // const searchInput = document.getElementById("search");
+
+  // searchInput.addEventListener("input", function () {
+  //   const query = this.value.toLowerCase();
+  //   const items = document.querySelectorAll(".swiper-slide poster");
+
+  //   items.forEach(item => {
+  //     console.log(item);
+  //     const name = item.dataset.name.toLowerCase();
+  //     if (name.includes(query)) {
+  //       item.classList.remove("hidden");
+  //     } else {
+  //       item.classList.add("hidden");
+  //     }
+    // });
+  // });
